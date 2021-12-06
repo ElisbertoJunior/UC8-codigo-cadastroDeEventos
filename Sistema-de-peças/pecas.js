0beaad2b219675636a7a7b55bbd7a2409a21844d
Elisberto Junior <elisberto_jr@hotmail.com>
2021-12-06 19:58:11 -0300

    add lista de pecas

---
+++ Sistema-de-peças/pecas.js
@@ -0,0 +1,23 @@
+var listaDePecas = ["Filtro de ar", "Radiador", "Amortecedor"]
+
+if(listaDePecas.length < 10){
+    console.log("E possivel cadastrar mais peças")
+} else {
+    console.log("Sem espaço para cadastrar")
+}
+
+let peso = 50;
+if(peso < 100){
+    console.log("A peça deve ter no minimo 100g")
+} else {
+    console.log("A peça possui peso adequado.")
+}
+
+let nomePecas = "Disco de freio"
+if(nomePeca.length > 3){
+    console.log("Nome da peça esta adequado para cadastro")
+} else if(nomePeca.length == 0){
+    console.log("O Nome da peça nao pode ser vazio.")
+} else {
+    console.log("Nome da peça nao esta adequado")
+}
